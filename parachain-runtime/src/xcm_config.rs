013feb1ee2bdfac572f038aee62ac43cc6f96742
Alejandro Martinez Andres <11448715+al3mart@users.noreply.github.com>
2025-04-10 16:29:40 +0200

    chore: sync with polkadot-stable2503

--- parachain-runtime/src/xcm_config.rs
+++ parachain-runtime/src/xcm_config.rs
@@ -4,7 +4,7 @@ use super::{
 };
 use frame_support::{
 	parameter_types,
-	traits::{ConstU32, Contains, Everything, Nothing},
+	traits::{ConstU32, Contains, Disabled, Everything, Nothing},
 	weights::Weight,
 };
 use frame_system::EnsureRoot;
@@ -193,6 +193,7 @@ impl pallet_xcm::Config for Runtime {
 	type AdminOrigin = EnsureRoot<AccountId>;
 	type MaxRemoteLockConsumers = ConstU32<0>;
 	type RemoteLockConsumerIdentifier = ();
+	type AuthorizedAliasConsideration = Disabled;
 }
 
 impl cumulus_pallet_xcm::Config for Runtime {
