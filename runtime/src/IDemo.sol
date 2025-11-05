f2ed5168bdcb59c19212d7ab51286f1257338a39
Michael Müller <mich@elmueller.net>
2025-11-05 06:43:46 +0100

    Fix comment
    Updated the IDemo interface to include a new notice for the echo function.

--- runtime/src/IDemo.sol
+++ runtime/src/IDemo.sol
@@ -9,7 +9,7 @@ address constant DEMO_PRECOMPILE_ADDRESS = address(0xB0000);
 /// @dev Documentation:
 /// @dev - ink! Contract calling this interface: https://github.com/use-ink/ink-examples/precompile-demo
 interface IDemo {
-    /// @notice Estimates the `Weight` required to execute a given XCM message.
+    /// @notice Simple echo function.
     /// @param mode The value `0` causes the `echo` function to revert.
     /// @return If `mode > 0`, the input `message` is echoed back to the caller.
     function echo(uint8 mode, bytes message) external view returns (bytes);
